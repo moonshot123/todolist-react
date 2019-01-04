92ddf4541b59f429a95d762f4c8117168292b64a
moonshot123 <whwjddn200@naver.com>
2019-01-04 21:37:51 +0900

    1-commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        일정관리
+      </div>
+    );
+  }
+}
+
+export default App;
